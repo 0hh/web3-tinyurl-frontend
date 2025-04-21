e76983a7456a3ea0ec6d9e4d21af08d5def632fa
0hh <mail2dimi@gmail.com>
2025-04-21 13:03:35 +0000

    Zustand for State Management

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import useStore from './store/useStore';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { count, increment, decrement } = useStore();
 
   return (
     <>
@@ -18,9 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <p>Count is {count}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
