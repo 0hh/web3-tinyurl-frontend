556be726527c061a9051f0b4efa65a996e75fdab
0hh <mail2dimi@gmail.com>
2025-04-21 15:11:49 +0000

    Style like a web3 site 👾

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css'; // Assuming you have some styles defined here
+import './components/LandingPage.css';
 import LandingPage from './components/LandingPage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <LandingPage />
-    </div>
+      <div>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300&display=swap"
+          rel="stylesheet"
+        />
+        <LandingPage />
+      
+        </div>
+        </div>
   );
 };
 
